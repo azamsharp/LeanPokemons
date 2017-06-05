8922a570a6c16a8df904991b81ef5d4fa92ab360
azamsharp <azamsharp@gmail.com>
2017-06-05 14:01:24 -0700

    setting up the base tab bar controller

--- LeanPokemons/AppDelegate.swift
+++ LeanPokemons/AppDelegate.swift
@@ -15,7 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        guard let rootVC = self.window?.rootViewController as? BaseTabBarViewController else {
+            fatalError("RootViewController not defined")
+        }
+        
+        rootVC.delegate = rootVC
+        
+        
         return true
     }
 
