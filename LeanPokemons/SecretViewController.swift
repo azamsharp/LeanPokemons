06aa0ef1985d7123afc1637f0ccc624404c6485a
azamsharp <azamsharp@gmail.com>
2017-06-06 07:22:46 -0700

    adding secret view controller

---
+++ LeanPokemons/SecretViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SecretViewController.swift
+//  LeanPokemons
+//
+//  Created by Mohammad Azam on 6/6/17.
+//  Copyright © 2017 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SecretViewController : UIViewController, LoginHandler {
+    
+}
